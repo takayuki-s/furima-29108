2534b4c0409241a9813d46f53cd60fdbb2ec18c5
takayuki-s <taka12yuki04@gmail.com>
2020-10-06 22:12:45 +0900

    Usersテーブルにカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_06_104237) do
+ActiveRecord::Schema.define(version: 2020_10_06_130630) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2020_10_06_104237) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "nick_name", null: false
+    t.string "family_name", null: false
+    t.string "family_name_reading", null: false
+    t.string "first_name", null: false
+    t.string "first_name_reading", null: false
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

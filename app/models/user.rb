3d8c13cd6e8fc1ccb2e6f58b6a4f54e5362fd7a9
takayuki-s <taka12yuki04@gmail.com>
2020-10-08 20:20:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nick_name
+    validates :password, length: { minimum: 6 },format: { with: /\A[a-z0-9]+\z/i}
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
 end

7a7b79b905753d5410a39bc025a3c599c1073466
takayuki-s <taka12yuki04@gmail.com>
2020-10-08 22:16:55 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nick_name
-    validates :password, length: { minimum: 6 },format: { with: /\A[a-z0-9]+\z/i}
+    validates :password, length: { minimum: 6 },format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/}
     validates :family_name_reading, format: { with: /\A[ァ-ヶー－]+\z/}
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nick_name              {"furima太郎"}
+    email                 {"sample@sample.com"}
+    password              {"a1a1a1"}
+    password_confirmation {"a1a1a1"}
+    family_name           {"山田"}
+    family_name_reading   {"ヤマダ"}
+    first_name            {"太郎"}
+    first_name_reading    {"タロウ"}
+    birthday              {"1930-01-01"}
+  end
+end

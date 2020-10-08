6ceaee6c149fbf66839aa43055bf4f084f8dca3f
takayuki-s <taka12yuki04@gmail.com>
2020-10-08 12:21:51 +0900

    itemsコントローラー生成、相互リンク

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'items/index'
+  get 'items/new'
   devise_for :users
+  root to: 'items#index'
+  resources :items
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

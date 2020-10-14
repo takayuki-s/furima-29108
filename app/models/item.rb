1886c93415179651d07682ec527a7da40a1d0278
takayuki-s <taka12yuki04@gmail.com>
2020-10-14 21:50:17 +0900

    AcctiveHashによるカテゴリ選択設定とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :days_to_ship, :delivery_charge, :shipment_source, :status
+
+  validates :name, :detail, :price, presemce: true
+
+  validates :category_id, :days_to_ship_id, :delivery_char_id, :shipment_source_id, :status_id, numericality: { other_than: 1 } 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  has_many :items
+  
   with_options presence: true do
     validates :nick_name
     validates :password, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }

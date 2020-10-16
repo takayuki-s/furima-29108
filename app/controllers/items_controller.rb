f933123c646c50e19b8cbe0b4036725f10803c60
takayuki-s <taka12yuki04@gmail.com>
2020-10-17 08:28:51 +0900

    ActiveStorageによる画像添付設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,10 @@ class ItemsController < ApplicationController
 
   def new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :detail, :price,:category_id, :days_to_ship_id, :delivery_char_id, :shipment_source_id, :status_id, :image).merge(user_id: current_user.id)
+  end
 end

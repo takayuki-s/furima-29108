c876ebdf02133db1812ad6fbb6836a08f6a346c7
takayuki-s <taka12yuki04@gmail.com>
2020-10-17 09:24:38 +0900

    ログインユーザーのみ出品できる設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+
   def index
+    @items = Item.all
   end
 
   def new
+    @item = Item.new
   end
 
   private
 
   def item_params
     params.require(:item).permit(:name, :detail, :price,:category_id, :days_to_ship_id, :delivery_char_id, :shipment_source_id, :status_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end

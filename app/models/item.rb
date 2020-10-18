c1d23256a846681830f3b236df4cdf7a2f293709
takayuki-s <taka12yuki04@gmail.com>
2020-10-18 20:13:42 +0900

    データ保存設定＋細かなミス修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,14 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :days_to_ship, :delivery_charge, :shipment_source, :status
+  belongs_to_active_hash :category
+  belongs_to_active_hash :days_to_ship
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :shipment_source
+  belongs_to_active_hash :status
 
-  validates :name, :detail, :price, presemce: true
-
-  validates :category_id, :days_to_ship_id, :delivery_char_id, :shipment_source_id, :status_id, numericality: { other_than: 1 } 
+  validates :name, :detail, :image, presence: true
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/ }
+  validates :category_id, :days_to_ship_id, :delivery_charge_id, :shipment_source_id, :status_id, presence: :true
 
 end

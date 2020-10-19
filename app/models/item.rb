096fe112284a7c03ab914d74a4a263f52e4824cd
takayuki-s <taka12yuki04@gmail.com>
2020-10-19 22:11:59 +0900

    rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,6 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
 
   validates :name, :detail, :image, presence: true
-  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 300, :less_than_or_equal_to => 9999999}, format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, format: { with: /\A[0-9]+\z/ }
   validates :category_id, :days_to_ship_id, :delivery_charge_id, :shipment_source_id, :status_id, presence: :true
-
 end

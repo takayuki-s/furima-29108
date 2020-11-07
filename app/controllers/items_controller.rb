30471ff88c1e4f3cb755cd33f0802aaff7dce852
takayuki-s <taka12yuki04@gmail.com>
2020-11-07 17:35:34 +0900

    出品ボタン遷移編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -47,7 +47,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   def set_item
